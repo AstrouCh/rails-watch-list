5eb6b0ceeca869798119ccd4cfc7992afe15546c
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-03-09 12:37:45 +0100

    spec model all green

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
-  has_many :lists
+  has_many :bookmarks
+  validates :title, presence: { message: "Title must be given please" }
+  validates :title, presence: true
+  validates :overview, presence: { message: "Overview must be given please" }
+
 end
